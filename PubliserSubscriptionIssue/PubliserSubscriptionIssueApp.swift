7e10b16f0e90e5c6464ac79b1a1056ecb12d5ba5
Ilia Kolo <ilia@kolo.ee>
2023-10-04 16:01:33 +0300

    Initial Commit

---
+++ PubliserSubscriptionIssue/PubliserSubscriptionIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PubliserSubscriptionIssueApp.swift
+//  PubliserSubscriptionIssue
+//
+//  Created by Ilia Kolomeitsev on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PubliserSubscriptionIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
